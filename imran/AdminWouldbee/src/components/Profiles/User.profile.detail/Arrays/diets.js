543e79baeaf9f1901036c61b2751809b03b8afdd
imranmandal <64127903+imranmandal@users.noreply.github.com>
2021-03-25 22:28:37 +0530

    Delete imran/AdminWouldbee directory

--- imran/AdminWouldbee/src/components/Profiles/User.profile.detail/Arrays/diets.js
+++
@@ -1,7 +0,0 @@
-export const Diets = [
-    'VEGAN',
-    'JAIN',
-    'VEGETARIAN',
-    'EGGETARIAN',
-    'NON_VEGETARIAN',
-]
--- imran/AdminWouldbee/src/components/Profiles/User.profile.detail/Arrays/social.categories.js
+++
@@ -1,9 +0,0 @@
-export const religionArray = [
-    'Hindu',
-    'Jain - Digamber',
-    'Jain - Shwetamber',
-    'Muslim - Shia',
-    'Muslim - Sunni',
-    'Chistian',
-    'Sikh'
-]
